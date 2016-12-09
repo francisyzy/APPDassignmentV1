d0b62fec729b728bad49702cda116e73755cf5fe
Garo <FENGYE.16@ichat.sp.edu.sg>
2016-12-09 13:18:12 +0900

    fh

--- APPDassignmentV1/House.cs
+++ APPDassignmentV1/House.cs
@@ -10,7 +10,7 @@ namespace APPDassignmentV1.House
     {
         
         public string resourceId { get; set; }
-        public string fullAddress { get; set; }
+        public address Address { get; set; }
         public double price { get; set; }
         public string region { get; set; }
 
@@ -20,7 +20,7 @@ namespace APPDassignmentV1.House
     {
         public int unitId { get; set; }
         public int houseSize { get; set; }
-        public string facilities { get; set; }
+        public List<facilities> Facility { get; set; }
         public int roomCount { get; set; }
         public int aircon { get; set; }
     }
@@ -30,5 +30,20 @@ namespace APPDassignmentV1.House
         public int roomId { get; set; }
         public int roomSize { get; set; }
         public Boolean aircon { get; set; }
+        
+    }
+
+    public class facilities
+    {
+        public int facilityTypeID { get; set; }
+        public string facilityTypeName { get; set; }
+        
+
+    }
+
+    public class address
+    {
+        public int postcode { get; set; }
+        public string fullAddress { get; set; }
     }
 }
