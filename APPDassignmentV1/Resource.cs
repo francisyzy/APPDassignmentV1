be742348fe34221028b7f5c14a711af9fdc05879
Francis Yeo Zhao Yik <Francis Yeo Zhao Yik@MACBOOKAIR11WIN>
2016-12-09 11:41:33 +0800

    Added resource/ resource.txt

--- APPDassignmentV1/Resource.cs
+++ APPDassignmentV1/Resource.cs
@@ -4,9 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace APPDassignmentV1
+namespace APPDassignmentV1.Resource
 {
     class Resource
     {
+
+    }
+    
+    class unitResource
+    {
+
+    }
+
+    class RoomResource
+    {
+
     }
 }
