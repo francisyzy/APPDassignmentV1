f769239e43d268b6d35047712c04f8157a0f7789
Francis Yeo Zhao Yik <Francis Yeo Zhao Yik@MACBOOKAIR11WIN>
2016-12-09 12:49:19 +0800

    codecopying

--- APPDassignmentV1/MainWindow.xaml.cs
+++ APPDassignmentV1/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
+using System.Text;
+using Newtonsoft.Json.Linq;
 
 namespace APPDassignmentV1
 {
@@ -23,6 +26,19 @@ namespace APPDassignmentV1
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<Resource> resourceList;
+
+            using(StreamReader file = file.OpenText(@"ResourceData.txt"))
+            {
+                using(JsonTextReader reader = new JsonTextReader(file))
+                {
+
+                }
+            }
         }
     }
 }
