62e9407ba8f62a4f5e9d8769bdad2368fd91cd2a
Francis Yeo Zhao Yik <Francis Yeo Zhao Yik@MACBOOKAIR11WIN>
2016-12-12 11:36:34 +0800

    user class added. user data added in txt form

---
+++ APPDassignmentV1/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APPDassignmentV1
+{
+    class User
+    {
+        public class user
+        {
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public string email { get; set; }
+            public string password { get; set; }
+            public int phoneNum { get; set; }
+            public DateTime birthday { get; set; }
+            public int creditCardNum { get; set; }
+            public DateTime creditCardExpiry { get; set; }
+            public int creditCardCVC { get; set; }
+        }
+
+    }
+}
