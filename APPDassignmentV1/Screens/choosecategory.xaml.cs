a3aa504f625e91239502b6b90ff0d42f913d3ac8
Francis Yeo Zhao Yik <Francis Yeo Zhao Yik@MACBOOKAIR11WIN>
2017-01-31 11:42:14 +0800

    Map Thingy V0

--- APPDassignmentV1/Screens/choosecategory.xaml.cs
+++ APPDassignmentV1/Screens/choosecategory.xaml.cs
@@ -1,23 +1,8 @@
-﻿using APPDassignmentV1;
-using APPDassignmentV1.Models;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+﻿using APPDassignmentV1.Models;
 using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Microsoft.EntityFrameworkCore;
 
 
 namespace APPDassignmentV1.Screens
